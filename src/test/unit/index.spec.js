290a0b22d04526695b06107aebf44b7d682215d1
Bhupendra Bhudia <bhupendra.bhudia@gmail.com>
2020-04-06 13:59:38 +0100

    Updates

--- src/test/unit/index.spec.js
+++
@@ -1,8 +0,0 @@
-var helpers = require('../helpers');
-var main = require('../../js/main');
-
-describe('main', function () {
-  it('returns hello world', function () {
-    helpers.expect(main()).to.eql('hello world');
-  });
-});
